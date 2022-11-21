22fa5410e01cb591792fac349f4004aa62a74b4b
wooseop <crazyz2000@naver.com>
2022-11-21 22:04:16 +0900

    Add Enum - Ability, SetEffect

---
+++ enum/ability.h
@@ -0,0 +1,65 @@
+#ifndef ABILITY_H
+#define ABILITY_H
+
+#include <QString>
+
+class Ability
+{
+public:
+    enum
+    {
+        Critical,
+        Specialization,
+        Swift,
+        Domination,
+        Endurance,
+        Expertise,
+        SIZE
+    };
+
+    static int size()
+    {
+        return SIZE;
+    }
+
+    static QString enumToKStr(int ability)
+    {
+        switch (ability)
+        {
+        case Critical:
+            return "치명";
+        case Specialization:
+            return "특화";
+        case Swift:
+            return "신속";
+        case Domination:
+            return "제압";
+        case Endurance:
+            return "인내";
+        case Expertise:
+            return "숙련";
+        default:
+            return "";
+        }
+    }
+
+    static int kstrToEnum(QString ability)
+    {
+        if ("치명" == ability)
+            return Critical;
+        else if ("특화" == ability)
+            return Specialization;
+        else if ("신속" == ability)
+            return Swift;
+        else if ("제압" == ability)
+            return Domination;
+        else if ("인내" == ability)
+            return Endurance;
+        else if ("숙련" == ability)
+            return Expertise;
+        else
+            return -1;
+    }
+};
+
+#endif // ABILITY_H
---
+++ enum/set_effect.h
@@ -0,0 +1,80 @@
+#ifndef SET_EFFECT_H
+#define SET_EFFECT_H
+
+#include <QString>
+
+class SetEffect
+{
+public:
+    enum
+    {
+        악몽,
+        구원,
+        사멸,
+        지배,
+        환각,
+        갈망,
+        배신,
+        매혹,
+        파괴,
+        SIZE
+    };
+
+    static int size()
+    {
+        return SIZE;
+    }
+
+    static QString enumToKStr(int setEffect)
+    {
+        switch (setEffect)
+        {
+        case 구원:
+            return "구원";
+        case 지배:
+            return "지배";
+        case 악몽:
+            return "악몽";
+        case 사멸:
+            return "사멸";
+        case 환각:
+            return "환각";
+        case 갈망:
+            return "갈망";
+        case 배신:
+            return "배신";
+        case 매혹:
+            return "매혹";
+        case 파괴:
+            return "파괴";
+        default:
+            return "";
+        }
+    }
+
+    static int kstrToEnum(QString setEffect)
+    {
+        if ("구원" == setEffect)
+            return 구원;
+        else if ("지배" == setEffect)
+            return 지배;
+        else if ("악몽" == setEffect)
+            return 악몽;
+        else if ("사멸" == setEffect)
+            return 사멸;
+        else if ("환각" == setEffect)
+            return 환각;
+        else if ("갈망" == setEffect)
+            return 갈망;
+        else if ("배신" == setEffect)
+            return 배신;
+        else if ("매혹" == setEffect)
+            return 매혹;
+        else if ("파괴" == setEffect)
+            return 파괴;
+        else
+            return -1;
+    }
+};
+
+#endif // SET_EFFECT_H
